a6af338f244a5dcf9d574314b65522752a806991
Alfonso Andrés López Molina <osnoser_12@hotmail.com>
2016-12-22 11:11:43 -0400

    - Fixed: Inteligencia activada adaptándola a los cambios actuales del código. - WIP: Solución de problemas de la IA. - Know issue: Debido a que la IA  mueve directamente al personaje, el mapa no se actualiza bien.

--- src/Personajes/Inteligencia.java
+++ src/Personajes/Inteligencia.java
@@ -5,170 +5,79 @@
 package Personajes;
 
 import gui.JPanelJuego;
-import java.awt.event.ActionEvent;
 import java.util.Random;
-import javax.swing.AbstractAction;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import javax.swing.Timer;
+import juego.constantes.Estado;
+import static juego.constantes.Estado.*;
 
 /**
  *
  * @author hp
  */
-
 public class Inteligencia {
-    
+
     private final Personaje personaje;
     private Timer timer;
     private Random random;
-    
-    public Inteligencia(Personaje personaje){
-        this.personaje=personaje;
-        random=new Random();
+
+    int a = 1, b = 1, time = 0;
+
+    public Inteligencia(Personaje personaje) {
+        this.personaje = personaje;
+        random = new Random();
         determinarInteligencia();
     }
 
     private void determinarInteligencia() {
-        /*
-         if(personaje.smart == personaje.SMART_LOW){
-         timer=new Timer(50,new AbstractAction(){
-         int a=1,time=0;
-         @Override
-         public void actionPerformed(ActionEvent e) {
-         time++;
-         if(time%50==0){
-         if(reconocimientoX())
-         a=getRandom().nextInt(2);
-         if(reconocimientoY())
-         a=getRandom().nextInt(2);
-         }
-         if(a==0) {
-         personaje.setEstadoActual(Personaje.Estado.IZQUIERDA);
-         personaje.movimientoIzquierda();
-         } else if(a==1) {
-         personaje.setEstadoActual(Personaje.Estado.DERECHA);
-         personaje.movimientoDerecha();
-         } else if(a==2) {
-         personaje.setEstadoActual(Personaje.Estado.ARRIBA);
-         personaje.movimientoArriba();
-         } else if(a==3) {
-         personaje.setEstadoActual(Personaje.Estado.ABAJO);
-         personaje.movimientoAbajo();
-         } if(personaje.getPosicionMapa().equals(JPanelJuego.getInstance(null).primerJugador().getPosicionMapa()) && !JPanelJuego.getInstance(null).primerJugador().getMYSTERY()){
-         timer.stop();
-         JPanelJuego.getInstance(null).borrarJugador();
-         }
-         }            
-         });
-        
-         }
-         else if(personaje.smart==personaje.SMART_MID){
-         timer=new Timer(50,new AbstractAction(){
-         int a=1,b=1,time=0;
-         @Override
-         public void actionPerformed(ActionEvent e) {
-         time++;
-         if(time%50==0){
-         if(reconocimientoX())
-         a=getRandom().nextInt(2);
-         if(reconocimientoY())
-         b=getRandom().nextInt(2);
-         }
-         if(a==0) {
-         personaje.setEstadoActual(Personaje.Estado.IZQUIERDA);
-         personaje.movimientoIzquierda();
-         } else if(a==1) {
-         personaje.setEstadoActual(Personaje.Estado.DERECHA);
-         personaje.movimientoDerecha();
-         }
-         if(b==0) {
-         personaje.setEstadoActual(Personaje.Estado.ARRIBA);
-         personaje.movimientoArriba();
-         }
-         else if(b==1) {
-         personaje.setEstadoActual(Personaje.Estado.ABAJO);
-         personaje.movimientoAbajo();
-         }
-         if(personaje.getPosicionMapa().equals(JPanelJuego.getInstance(null).primerJugador().getPosicionMapa()) && !JPanelJuego.getInstance(null).primerJugador().getMYSTERY()){
-         timer.stop();
-         JPanelJuego.getInstance(null).borrarJugador();
-         }
-         }            
-         });
-      
-         }
-         else if(personaje.smart==personaje.SMART_HIGH){
-         timer=new Timer(50,new AbstractAction(){
-         int a=1,b=1,time=0;
-         @Override
-         public void actionPerformed(ActionEvent e) {
-         time++;
-         if(time%50==0){
-         if(reconocimientoX())
-         a=getRandom().nextInt(2);
-         if(reconocimientoY())
-         b=getRandom().nextInt(2);
-         }
-         if(a==0) {
-         personaje.setEstadoActual(Personaje.Estado.IZQUIERDA);
-         personaje.movimientoIzquierda();
-         }
-         else if(a==1) {
-         personaje.setEstadoActual(Personaje.Estado.DERECHA);
-         personaje.movimientoDerecha();
-         }
-         if(b==0) {
-         personaje.setEstadoActual(Personaje.Estado.ARRIBA);
-         personaje.movimientoArriba();
-         }
-         else if(b==1) {
-         personaje.setEstadoActual(Personaje.Estado.ABAJO);
-         personaje.movimientoAbajo();
-         }
-         if(personaje.getPosicionMapa().equals(JPanelJuego.getInstance(null).primerJugador().getPosicionMapa()) && !JPanelJuego.getInstance(null).primerJugador().getMYSTERY()){
-         timer.stop();
-         JPanelJuego.getInstance(null).borrarJugador();
-         }
-         }            
-         });
-         }
-         */
+        timer = personaje.smart == Personaje.SMART_LOW ? new Timer(50, e -> {
+            if (++time % 50 == 0) {
+                a = ((a = random.nextInt(2)) == 0
+                        ? IZQUIERDA : a == 1 ? DERECHA : a == 2 ? ARRIBA : ABAJO).val();
+            }
+            movimientoEje(a, JPanelJuego.getInstance(null));
+            if (JPanelJuego.getInstance(null).primerJugador().getEstadoActual() == MUERTE.val()) {
+                timer.stop();
+            }
+        }) : personaje.smart == Personaje.SMART_MID || personaje.smart == Personaje.SMART_HIGH ? new Timer(50, e -> {
+            if (++time % 50 == 0) {
+                    a = (random.nextInt(2) == 0 ? IZQUIERDA : DERECHA).val();
+                    b = (random.nextInt(2) == 0 ? ARRIBA : ABAJO).val();
+            }
+            movimientoEje(a, JPanelJuego.getInstance(null));
+            movimientoEje(b, JPanelJuego.getInstance(null));
+            if (JPanelJuego.getInstance(null).primerJugador().getEstadoActual() == MUERTE.val()) {
+                timer.stop();
+            }
+        }) : null;
     }
 
     public Timer getTimer() {
         return timer;
     }
-    
+
     public void detenerInteligencia() {
-        if (timer == null)
+        if (timer == null) {
             return;
+        }
         timer.stop();
     }
-    
+
     public void iniciar() {
-        if (timer == null)
+        if (timer == null) {
             return;
+        }
         timer.start();
     }
-    
-//    public boolean reconocimientoX(){
-//        return (personaje.ChoqueDerecha("V", 1)||personaje.ChoqueIzquierda("V", 1));
-//    }
-//    public boolean reconocimientoY(){
-//        return (personaje.ChoqueArriba("V", 1)||personaje.ChoqueAbajo("V", 1));
-//    }
 
-    /**
-     * @return the random
-     */
-    public Random getRandom() {
-        return random;
-    }
-
-    /**
-     * @param random the random to set
-     */
-    public void setRandom(Random random) {
-        this.random = random;
+    private void movimientoEje(int direccion, JPanelJuego jPanelJuego) {
+        personaje.setEstadoActual(direccion);
+        Consumer<JPanelJuego> f = (direccion == ARRIBA.val() ? personaje::movimientoArriba
+                : direccion == ABAJO.val() ? personaje::movimientoAbajo
+                : direccion == DERECHA.val() ? personaje::movimientoDerecha
+                : personaje::movimientoIzquierda);
+        f.accept(jPanelJuego);
     }
 
 }
